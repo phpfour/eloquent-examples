a8e054eee5dd71c2654e8158e76c4473aa50e632
Mohammad Emran <phpfour@gmail.com>
2024-07-12 10:08:39 +0600

    Eager load user to solve n+1 query issue

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::query()
+            ->with(['user'])
             ->orderBy('published_at', 'desc')
             ->paginate(50);
 
